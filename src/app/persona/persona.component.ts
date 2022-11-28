e524b5514e26460a821a43c198de50ecdaabea72
JulianF <juliandavidbf@gmail.com>
2022-11-28 11:19:03 -0500

    @feat: creando inputs

---
+++ src/app/persona/persona.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { Persona } from '../persona.model';
+
+@Component({
+  selector: 'app-persona',
+  templateUrl: './persona.component.html',
+  styleUrls: ['./persona.component.scss']
+})
+export class PersonaComponent {
+
+
+  @Input() persona: Persona;
+  @Input() indice: number;
+  @Input() firstName: string;
+
+}
